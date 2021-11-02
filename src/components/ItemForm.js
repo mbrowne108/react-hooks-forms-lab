ce1dee025792995654d61dff9fe31f56ecf7ee9d
mbrowne108 <mbrowne108@gmail.com>
2021-11-02 13:02:35 -0400

    commit tree 0519e5a172d5f18783d76b5835fb7392ed09e075

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { v4 as uuid } from "uuid";
 
-function ItemForm(props) {
+function ItemForm({onItemFormSubmit}) {
+  const [formData, setFormData] = useState({
+    name: '',
+    category: 'Produce'
+  })
+
+  function handleChange(event) {
+    const name = event.target.name
+    setFormData({
+      ...formData,
+      [name]: event.target.value
+    })
+  }
+
   return (
-    <form className="NewItem">
+    <form className="NewItem" onSubmit={(event) => onItemFormSubmit(event, {...formData, id: uuid()})}>
       <label>
         Name:
-        <input type="text" name="name" />
+        <input type="text" name="name" onChange={handleChange} value={formData.name}/>
       </label>
 
       <label>
         Category:
-        <select name="category">
+        <select name="category" onChange={handleChange} value={formData.category}>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
           <option value="Dessert">Dessert</option>
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,23 +3,35 @@ import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
 
-function ShoppingList({ items }) {
+function ShoppingList({ items, onItemFormSubmit }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchText, setSearchText] = useState('search');
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
 
-  const itemsToDisplay = items.filter((item) => {
+  function handleSearchText(event) {
+    setSearchText(event.target.value);
+  }
+
+  let itemsToDisplay = items.filter((item) => {
     if (selectedCategory === "All") return true;
 
-    return item.category === selectedCategory;
+    return item.category === selectedCategory
   });
+
+  itemsToDisplay = itemsToDisplay.filter((item) => {
+    if (searchText === "search") return true;
+    return(item.name.toLowerCase().includes(searchText.toLowerCase()))
+  })
+
+
 
   return (
     <div className="ShoppingList">
-      <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <ItemForm onItemFormSubmit={onItemFormSubmit} items={items}/>
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchText}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
